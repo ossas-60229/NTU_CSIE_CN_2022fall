7dd14a5e5fc2017ec85993bb91141bba91fb9f82
ossas-60229 <ossas02066@LAPTOP-72G60OQ9>
2022-12-09 21:01:32 +0800

    31246

--- B08902066_hw3/openCV.cpp
+++ B08902066_hw3/openCV.cpp
@@ -1,18 +1,26 @@
+
+
 #include "opencv2/opencv.hpp"
 
-#include <fstream>
 #include <iostream>
+
 using namespace std;
 using namespace cv;
 
 int main(int argc, char *argv[]) {
     Mat server_img, client_img;
-    ifstream in;
-    in.open(argv[1]);
-    printf("open %s\n", argv[1]);
+    if (argc != 2) {
+        fprintf(stderr, "play error");
+    }
+    fprintf(stderr, "before capture %s\n", argv[1]);
+    VideoCapture cap(argv[1]);
+    fprintf(stderr, "after capture\n");
+
+    // Get the resolution of the video
+    int width = cap.get(CAP_PROP_FRAME_WIDTH);
+    int height = cap.get(CAP_PROP_FRAME_HEIGHT);
+
     // Allocate container to load frames
-    int width = atoi(argv[2]);
-    int height = atoi(argv[3]);
     server_img = Mat::zeros(height, width, CV_8UC3);
     client_img = Mat::zeros(height, width, CV_8UC3);
 
@@ -20,18 +28,25 @@ int main(int argc, char *argv[]) {
     if (!server_img.isContinuous()) {
         server_img = server_img.clone();
     }
+
     if (!client_img.isContinuous()) {
         client_img = client_img.clone();
     }
+
+    fprintf(stderr, "before loop\n");
     while (1) {
-        if (in.eof()) break;
+        // Get a frame from the video to the container of the server.
+        cap >> server_img;
+        if (server_img.empty()) break;
+        // Get the size of a frame in bytes
         int imgSize = server_img.total() * server_img.elemSize();
 
         // Allocate a buffer to load the frame (there would be 2 buffers in the
         // world of the Internet)
-        uchar *buffer = new uchar[imgSize];
-        in >> buffer;
-        // Get the size of a frame in bytes
+        char buffer[imgSize];
+
+        // Copy a frame to the buffer
+        memcpy(buffer, server_img.data, imgSize);
 
         // Here, we assume that the buffer is transmitted from the server to the
         // client Copy a frame from the buffer to the container of the client
@@ -45,10 +60,11 @@ int main(int argc, char *argv[]) {
         // Notice: this part is necessary due to openCV's design.
         // waitKey function means a delay to get the next frame. You can change
         // the value of delay to see what will happen
-        char c = (char)waitKey(33);
+        char c = (char)waitKey(1000);
         if (c == 27) break;
     }
 
+    cap.release();
     destroyAllWindows();
     return 0;
 }
